3b99ece5a3db987d57959548b7401bf319307c43
Amir Rezaie <40941371+AmirRezaie@users.noreply.github.com>
2021-11-30 18:11:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btn = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
+//Show The Modal Window 
 for (let i = 0; i < btnsShowModal.length; i++) {
   btnsShowModal[i].addEventListener("click", function () {
     console.log("button clicked!");
@@ -10,25 +11,36 @@ for (let i = 0; i < btnsShowModal.length; i++) {
     overlay.classList.remove("hidden");
   });
 }
+//Change Background, Title and Paragraph For First Button
 document.querySelector(".realmadrid").addEventListener("click", function () {
   body.style.backgroundImage = "url(images/santiagobernabeu.jpg)";
   document.querySelector("h1").textContent = "Real Madrid";
   document.querySelector("p").textContent =
     "Real Madrid Club de Fútbol, meaning Royal Madrid Football Club), commonly referred to as Real Madrid, is a Spanish professional football club based in Madrid.";
 });
+//Change Background, Title and Paragraph For Second Button
 document.querySelector(".arsenal").addEventListener("click", function () {
   body.style.backgroundImage = "url(images/emirates.jpg)";
   document.querySelector("h1").textContent = "Arsenal";
   document.querySelector("p").textContent =
     "Arsenal Football Club is a professional football club based in London, England. Arsenal plays in the Premier League, the top flight of English football. The club has won 13 league titles (including one unbeaten title), a record 14 FA Cups, two League Cups, 16 FA Community Shields, the League Centenary Trophy, one European Cup Winners' Cup, and one Inter-Cities Fairs Cup.";
 });
+//Change Background, Title and Paragraph For Third Button
 document.querySelector(".newcastle").addEventListener("click", function () {
   body.style.backgroundImage = "url(images/saintjamespark.jpg)";
   document.querySelector("h1").textContent = "Newcastle United";
   document.querySelector("p").textContent =
     "Newcastle United Football Club is an English professional football club based in Newcastle upon Tyne, that plays in the Premier League – the top flight of English football. The club was founded in 1892 by the merger of Newcastle East End and Newcastle West End. The team plays its home matches at St James' Park in the centre of Newcastle. Following the Taylor Report's requirement that all Premier League clubs have all-seater stadiums, the ground was modified in the mid-1990s and currently has a capacity of 52,305.";
 });
+//Close The Modal Window, When Click On X Button
 btn.addEventListener("click", function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+});
+//Close The Modal Window, When Press 'ESC' Button On Keyboard
+document.addEventListeners("keydown", function(e){
+  if(e.key === "Escape"){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
 });
